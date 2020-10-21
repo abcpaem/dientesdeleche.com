f2bdba52d5f528e8eae64e06bdeb0a0332ff3cd7
Patrick Espinosa <patrick.espinosamora@ticketmaster.co.uk>
2020-10-21 14:46:17 +0100

    add styles for headers and re-add address fields for physical products

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -52,13 +52,6 @@
     /* WooCommerce: The Code Below Removes Checkout Fields */
     add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
     function custom_override_checkout_fields( $fields ) {
-        unset($fields['billing']['billing_company']);
-        unset($fields['billing']['billing_address_1']);
-        unset($fields['billing']['billing_address_2']);
-        unset($fields['billing']['billing_city']);
-        unset($fields['billing']['billing_postcode']);
-        unset($fields['billing']['billing_state']);
-        unset($fields['billing']['billing_phone']);
         unset($fields['order']['order_comments']);
         return $fields;
     }
