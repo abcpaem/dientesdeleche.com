a2c2cf3432350c1e907d4f7a748e2eee399d2b31
Patrick Espinosa <patrick.espinosamora@ticketmaster.co.uk>
2020-12-11 09:07:49 +0000

    preload fonts

--- wp-content/themes/hello-elementor-child/header.php
+++ wp-content/themes/hello-elementor-child/header.php
@@ -17,6 +17,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
 	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<link rel="preload" as="font" href="/fonts/DeliusSwashCaps-Regular.woff" type="font/woff" crossorigin="anonymous">
+	<link rel="preload" as="font" href="/fonts/fontawesome.woff" type="font/woff" crossorigin="anonymous">
+	<meta property="fb:pages" content="113528133709348" />
+	<meta name="p:domain_verify" content="40373f5546f11fd345062f5124aee86d"/>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
